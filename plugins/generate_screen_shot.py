c7ce7e71b1a15d3bfa04d0d40b84f8a10ad5661c
Sahad.p <56154538+sahadp@users.noreply.github.com>
2020-04-07 17:55:00 +0530

    Update generate_screen_shot.py

--- plugins/generate_screen_shot.py
+++ plugins/generate_screen_shot.py
@@ -83,7 +83,7 @@ async def generate_screen_shot(bot, update):
             media_album_p = []
             if images is not None:
                 i = 0
-                caption = "© @AnyDLBot"
+                caption = "© @MAL4Y4LI"
                 for image in images:
                     if os.path.exists(image):
                         if i == 0:
